6f75f7378c48f706c98e5eaebc2150844e03a57e
DemiDaemi <daemihex@gmail.com>
2023-07-19 14:52:49 -0400

    update elo calculator

--- src/elo_calculator.py
+++ src/elo_calculator.py
@@ -7,32 +7,19 @@ class EloCalculator:
         # player can gain or lose in a single match
         self.k_factor = k_factor
 
-    def calculate_expected_score(self, player_rating: float, opponent_rating: float):
+    def calculate_expected_score(self, w_rating: float, l_rating: float):
         # Expected score is the probability of a player winning
         # based on their rating compared to their opponent's rating
-        expected_score = 1 / (1 + 10 ** ((opponent_rating - player_rating) / 400))
+        expected_score = 1 / (1 + 10 ** ((l_rating - w_rating) / 400))
         return expected_score
 
-    def update_ratings(self, player: Player, opponent: Player, player_won: bool):
-        player_expected_score = self.calculate_expected_score(
-            player.rating, opponent.rating
-        )
-        opponent_expected_score = self.calculate_expected_score(
-            opponent.rating, player.rating
-        )
-
-        if player_won:
-            player_result = 1
-            opponent_result = 0
-        else:
-            player_result = 0
-            opponent_result = 1
+    def update_ratings(self, w_player: Player, l_player: Player):
+        w_score = self.calculate_expected_score(w_player.rating, l_player.rating)
+        l_score = self.calculate_expected_score(l_player.rating, w_player.rating)
 
         # Calculate rating change using k-factor
-        player_rating_change = self.k_factor * (player_result - player_expected_score)
-        opponent_rating_change = self.k_factor * (
-            opponent_result - opponent_expected_score
-        )
+        w_rating_change = self.k_factor * (1 - w_score)
+        l_rating_change = self.k_factor * (0 - l_score)
 
-        player.update_rating(player.rating + player_rating_change)
-        opponent.update_rating(opponent.rating + opponent_rating_change)
+        w_player.update_rating(w_player.rating + w_rating_change)
+        l_player.update_rating(l_player.rating + l_rating_change)
--- tests/test_elo_calculator.py
+++ tests/test_elo_calculator.py
@@ -10,10 +10,11 @@ class TestEloCalculator(unittest.TestCase):
         expected_score_1 = elo_calculator.calculate_expected_score(1000, 1200)
         expected_score_2 = elo_calculator.calculate_expected_score(1200, 1000)
 
+        # Sum of two scores should be 1
         self.assertAlmostEqual(expected_score_1 + expected_score_2, 1.0, places=2)
-        self.assertGreater(
-            expected_score_2, expected_score_1
-        )  # Player with higher elo has higher expected chance to win
+
+        # Player with higher elo has higher expected chance to win
+        self.assertGreater(expected_score_2, expected_score_1)
 
     def test_update_ratings(self):
         player1 = Player(
@@ -25,11 +26,14 @@ class TestEloCalculator(unittest.TestCase):
         elo_calculator = EloCalculator()
 
         # Player 1 wins, rating should climb
-        elo_calculator.update_ratings(player1, player2, True)
+        elo_calculator.update_ratings(player1, player2)
         self.assertGreater(player1.rating, 1000)
 
         # Player 2 lost, rating should drop
         self.assertLess(player2.rating, 1200)
+
+        print(f"Player 1 rating: {player1.rating}")
+        print(f"Player 2 rating: {player2.rating}")
 
 
 if __name__ == "__main__":
