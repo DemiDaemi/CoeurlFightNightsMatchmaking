90a69c991c8977982b5383b7ff04e50e13a26ea3
DemiDaemi <daemihex@gmail.com>
2023-07-19 13:23:42 -0400

    Added types on players

--- src/player.py
+++ src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(
-        self, name, discord_account_id, ffxiv_character_id, world, initial_rating=1000
+        self,
+        name: str,
+        discord_account_id: str,
+        ffxiv_character_id: str,
+        world: str,
+        initial_rating: float = 1000,
     ):
         self.name = name
         self.discord_account = discord_account_id
@@ -8,8 +13,8 @@ class Player:
         self.world = world
         self.rating = initial_rating
 
-    def update_rating(self, new_rating):
+    def update_rating(self, new_rating: float):
         self.rating = new_rating
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Player: {self.name}, World: {self.world}, FFXIV: {self.ffxiv_character}, Rating: {self.rating}"
