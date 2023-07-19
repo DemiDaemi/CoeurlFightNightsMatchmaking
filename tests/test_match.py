249e896df8729368db8f2d01e4090b21acb3aa8b
DemiDaemi <daemihex@gmail.com>
2023-07-19 14:30:13 -0400

    Match tests

---
+++ tests/test_match.py
@@ -0,0 +1,40 @@
+import unittest
+from src.player import Player
+from src.match import Match
+
+
+class TestMatch(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Player("Player 1", "discord1", "character1", "world1", 1000)
+        self.player2 = Player("Player 2", "discord2", "character2", "world2", 1200)
+        self.match = Match(self.player1, self.player2, 3)
+
+    def test_create_match(self):
+        match = Match(self.player1, self.player2, 3)
+
+        self.assertEqual(match.player1, self.player1)
+        self.assertEqual(match.player2, self.player2)
+        self.assertEqual(match.result, (0, 0))
+        self.assertEqual(match.rounds, 3)
+        self.assertEqual(match.match_winner, None)
+
+    def test_set_results_1(self):
+        # Player 1 wins round 1
+        self.match.set_results(self.player1)
+        self.assertEqual(self.match.result, (1, 0))
+        self.assertEqual(self.match.match_winner, None)
+        print(self.match)
+
+        # Player 2 wins round 2
+        self.match.set_results(self.player2)
+        self.assertEqual(self.match.result, (1, 1))
+        self.assertEqual(self.match.match_winner, None)
+
+        # Player 1 wins round 3 (and the set)
+        self.match.set_results(self.player1)
+        self.assertEqual(self.match.result, (2, 1))
+        self.assertEqual(self.match.match_winner, self.player1)
+
+
+if __name__ == "__main__":
+    unittest.main()
