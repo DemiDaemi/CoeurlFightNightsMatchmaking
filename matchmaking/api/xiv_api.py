faa05030d7b88077c8b668ebfa29ded188b70ed4
Dae <daemihex@gmail.com>
2023-07-20 01:08:55 -0400

    Update XIV_API

--- matchmaking/api/xiv_api.py
+++ matchmaking/api/xiv_api.py
@@ -1,37 +1,55 @@
 import asyncio
 import logging
+import requests
 
 import pyxivapi
 from pyxivapi.models import Filter, Sort
 from matchmaking.api.config import XIV_API_KEY
 
-client = pyxivapi.XIVAPIClient(api_key=XIV_API_KEY)
-
 
 async def fetch_character_by_id(lodestone_id: int):
-    character = await client.character_by_id(lodestone_id=lodestone_id)
-    # Check if bio contains
-
-    await client.session.close()
-    return character
+    client = pyxivapi.XIVAPIClient(api_key=XIV_API_KEY)
+    character = None
+    try:
+        character = await client.character_by_id(lodestone_id=lodestone_id)
+    except Exception as e:
+        logging.error(f"An error occurred when fetching character: {e}")
+    finally:
+        await client.session.close()
+        return character
 
 
 async def fetch_character_by_name(forename: str, surname: str, world: str):
-    character = await client.character_search(
-        forename=forename, surname=surname, world=world
-    )
-    await client.session.close()
-    return character
+    client = pyxivapi.XIVAPIClient(api_key=XIV_API_KEY)
+    character = None
+    try:
+        character = await client.character_search(
+            forename=forename, surname=surname, world=world
+        )
+    except Exception as e:
+        logging.error(f"An error occurred when fetching character: {e}")
+    finally:
+        await client.session.close()
+        return character
 
 
 async def fetch_bio_by_lodestone_id(lodestone_id: int):
-    bio = await client.character_by_id(lodestone_id=lodestone_id)["Character"]["Bio"]
-    await client.session.close()
+    # client = pyxivapi.XIVAPIClient(api_key=XIV_API_KEY)
+    url = f"https://na.finalfantasyxiv.com/lodestone/character/{lodestone_id}/"
+    htmltext = requests.get(url).text
+    if htmltext.find("character__selfintroduction") == -1:
+        return None
+    bio = htmltext[
+        htmltext.find("character__selfintroduction")
+        + 29 : htmltext.find("character__selfintroduction")
+        + 1000
+    ]
+    bio = bio[: bio.find("</div>")]
     return bio
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(message)s", datefmt="%H:%M")
     loop = asyncio.get_event_loop()
-    character = loop.run_until_complete(fetch_character_by_name("Demi", "Daemi", "asd"))
+    character = loop.run_until_complete(fetch_bio_by_lodestone_id(11111111111111))
     print(character)
