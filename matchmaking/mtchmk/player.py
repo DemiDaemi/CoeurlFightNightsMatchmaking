b6ce6705e9452f29e14dfcdd5b63ad6d068e0f53
Dae <daemihex@gmail.com>
2023-07-20 00:12:18 -0400

    Update player.py to SQLAlchemy compliant

--- matchmaking/mtchmk/player.py
+++ matchmaking/mtchmk/player.py
@@ -1,20 +1,32 @@
-class Player:
-    def __init__(
-        self,
-        name: str,
-        discord_account_id: str,
-        ffxiv_character_id: str,
-        world: str,
-        initial_rating: float = 1000,
-    ):
-        self.name = name
-        self.discord_account = discord_account_id
-        self.ffxiv_character = ffxiv_character_id
-        self.world = world
-        self.rating = initial_rating
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    discord_account_id = Column(String)
+    ffxiv_character_id = Column(String)
+    world = Column(String)
+    rating = Column(Float)
+
+    matches = relationship("Match", back_populates="players")
 
     def update_rating(self, new_rating: float):
         self.rating = new_rating
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.name} - {self.world} ({self.rating})"
+
+
+class Verification(Base):
+    __tablename__ = "verifications"
+
+    id = Column(Integer, primary_key=True)
+    character_id = Column(String)
+    verification_code = Column(String)
